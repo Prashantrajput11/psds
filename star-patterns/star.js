2ab4fc712a19ce410fea7b507337053e2800cacd
Prashant Tanwar <69415658+Prashantrajput11@users.noreply.github.com>
2023-03-22 17:01:16 +0530

    pattern 4 done

--- star-patterns/star.js
+++ star-patterns/star.js
@@ -1,6 +1,36 @@
-for (let i = 0; i < 5; i++) {
-	for (let j = 0; j < 5; j++) {
-		process.stdout.write("*");
-	}
-	process.stdout.write("\n");
-}
+// pattern 1
+// for (let i = 0; i < 5; i++) {
+// 	for (let j = 0; j < 5; j++) {
+// 		process.stdout.write("*");
+// 	}
+// }
+
+// pattern 2
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// let rows = 9;
+// for (let i = 0; i < rows; i++) {
+// 	for (let j = 0; j <= i; j++) {
+// 		process.stdout.write("*");
+// 	}
+// 	process.stdout.write("\n");
+// }
+
+// pattern 4
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// pattern 4
+// let rows = 5;
+// for (let i = 1; i <= rows; i++) {
+// 	for (let j = 1; j <= i; j++) { // prints row number i.e i
+// 		process.stdout.write(i.toString());
+// 	}
+// 	process.stdout.write("\n");
+// }
