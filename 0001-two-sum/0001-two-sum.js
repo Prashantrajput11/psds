897066def8cc76318e5631fc7b8d7188a46abac6
Prashant Tanwar <69415658+Prashantrajput11@users.noreply.github.com>
2023-09-19 21:42:48 +0530

    Time: 58 ms (75.02%), Space: 41.9 MB (91.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,16 +3,31 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let indices = []
-    for(let i = 0; i < nums.length;i++){
+// var twoSum = function(nums, target) {
+//     let indices = []
+//     for(let i = 0; i < nums.length;i++){
 
-        for(j = i+1; j <nums.length;j++){
-            if(nums[i] + nums[j]=== target){
-                 indices.push(i, j)
-            }
-        }
-    }
+//         for(j = i+1; j <nums.length;j++){
+//             if(nums[i] + nums[j]=== target){
+//                  indices.push(i, j)
+//             }
+//         }
+//     }
 
-    return indices
-};
+//     return indices
+// };
+
+var twoSum = function(arr, target) {
+	let map = {};
+
+	for (let i = 0; i < arr.length; i++) {
+		let diff = target - arr[i];
+
+		if (map.hasOwnProperty(diff)) {
+			return [map[diff], i];
+		} else {
+			map[arr[i]] = i;
+		}
+	}
+}
+
